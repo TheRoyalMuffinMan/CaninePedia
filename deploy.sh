7c6867f3281e2843f0ed150841b180c7ddd40824
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-08-22 11:19:14 -0500

    push to heroku

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,8 @@ npm run build
 cd ..
 mv client/build/ server/pages
 
-cd /server
 git add .
 git commit --allow-empty -m "redeploying"
-git push -f heroku main
+git push heroku main
 
 rm -r server/pages
