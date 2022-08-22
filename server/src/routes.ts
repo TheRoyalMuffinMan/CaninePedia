428906a591a4d27f85115db930a627c60f112d6f
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-08-22 11:04:33 -0500

    Setting repo

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const path = require('path');
+const port = process.env.PORT || 3011;
+
+app.use(express.static(path.join(__dirname, "..", "/pages")))
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "..", "/pages", "index.html"));
+});
+
+app.get('/yes', (req: Request, res: Response) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
