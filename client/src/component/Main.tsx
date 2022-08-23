7954ce239089fda46d194dcab535260274cd367c
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-08-22 20:51:43 -0500

    redeploying

--- client/src/component/Main.tsx
+++ client/src/component/Main.tsx
@@ -1,70 +1,95 @@
-import { Flex, Stack, Box, AspectRatio, Image, Heading, Text } from "@chakra-ui/react";
+import { Flex, Box, Image, Heading, Text } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 import { Select } from "chakra-react-select";
 import { SiteTheme, defaultImage } from "../util/global";
-const options = [
-    { label: 'The Godfather', id: 1 },
-    { label: 'Pulp Fiction', id: 2 },
-    { label: 'Pulp Fiction', id: 2 },
-    { label: 'Pulp Fiction', id: 2 },
-    { label: 'Pulp Fiction', id: 2 },
-    { label: 'Pulp Fiction', id: 2 },
-    { label: 'Pulp Fiction', id: 2 },
-    { label: 'Pulp Fiction', id: 2 },
-];
+
+type Breed = {
+  label: string,
+  value: string,
+}
 
 const Main = () => {
+  const [breeds, setBreeds]: [Breed[] | undefined, Function] = useState<Breed[] | undefined>(undefined)
+
+  useEffect(() => {
+    const fetchDogList = async () => {
+      const response = await fetch("/doglist");
+      const result = (await response.json())["message"];
+      let dogs: Breed[] = []
+      
+      Object.keys(result).forEach((breed: string) => {
+        if (result[breed].length > 0) {
+
+          result[breed].forEach((subBreed: string) => {
+            dogs.push({
+              label: breed + ' ' + subBreed,
+              value: breed + ' ' + subBreed
+            })
+          })
+
+        } else {
+
+          dogs.push({
+            label: breed,
+            value: breed
+          })
+
+        }
+      });
+
+      setBreeds(dogs);
+    }
+
+    fetchDogList().catch(console.error);;
+  }, [])
+
+  if (breeds === undefined) {
     return (
-        <Flex 
-            flex="1"
-            flexDirection="column"
-            justify="center"
-            align="center"
-            padding="2rem"
-            gap="1rem"
-        >
-            <Heading 
-                fontSize={["1rem", "1.5rem", "2rem", "3rem", "4rem"]}
-                fontWeight="black"
-                color={SiteTheme.green}
-            >
-                The Search Is Over
-            </Heading>
-            <Text
-                fontSize={["0.6rem", "0.8rem", "0.8rem", "1.5rem", "2rem"]}
-                fontWeight="light"
-                color="black"
-            >
-                Simply select the dog breed and let us find the image
-            </Text>
-            <Box width={["500px"]}>
-                <Select
-                    hasStickyGroupHeaders
-                    placeholder="Select a Option"
-                    size="lg"
-                    focusBorderColor={SiteTheme.green}
-                    colorScheme= "red"
-                    errorBorderColor="red"
-                    useBasicStyles={true}
-                    options={[
-                        {
-                          label: "I am red",
-                          value: "i-am-red",
-                          colorScheme: "red", // The option color scheme overrides the global
-                        },
-                        {
-                          label: "I fallback to purple",
-                          value: "i-am-purple",
-                        },
-                      ]}
-                />
-            </Box>
-            <Image
-                width="1024px"
-                height="768px"
-                src={defaultImage}
-            />
-        </Flex>
+      <Heading>Waiting....</Heading>
     );
+  }
+
+  return (
+    <Flex 
+      flex="1"
+      flexDirection="column"
+      justify="center"
+      align="center"
+      padding="2rem"
+      gap="1rem"
+    >
+      <Heading 
+        fontSize={["1rem", "1.5rem", "2rem", "3rem", "4rem"]}
+        fontWeight="black"
+        color={SiteTheme.green}
+      >
+        The Search Is Over
+      </Heading>
+      <Text
+        fontSize={["0.6rem", "0.8rem", "0.8rem", "1.5rem", "2rem"]}
+        fontWeight="light"
+        color="black"
+      >
+        Simply select the dog breed and let us find the image
+      </Text>
+      <Box width={["500px"]}>
+        <Select
+          hasStickyGroupHeaders
+          placeholder="Select a Option"
+          size="lg"
+          focusBorderColor={SiteTheme.green}
+          errorBorderColor="red"
+          useBasicStyles={true}
+          options={breeds}
+        />
+      </Box>
+      <Image
+        width="1024px"
+        height="768px"
+        src={defaultImage}
+      />
+    </Flex>
+  );
 }
 
 export default Main;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,23 +1,26 @@
 import express, { Express, Request, Response } from 'express';
+import fetch from 'cross-fetch';
 
-const app: Express = express();
+const router: Express = express();
 const path = require('path');
 const port = process.env.PORT || 3011;
 
-app.use(express.static(path.join(__dirname, "..", "/pages")))
+router.use(express.static(path.join(__dirname, "..", "/pages")))
 
-app.get('/', (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
   res.sendFile(path.resolve(__dirname, "..", "/pages", "index.html"));
 });
 
-// app.get('/doglist', (req: Request, res: Response)) => {
+router.get('/doglist', async (req: Request, res: Response) => {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all")
 
-// }
+  if (!response.ok) {
+    return res.json({ msg: `Error! Request Code: ${response.statusText}` })
+  }
 
-app.get('/yes', (req: Request, res: Response) => {
-  res.send("hello");
+  response.json().then(dogs => res.send(dogs));
 });
 
-app.listen(port, () => {
+router.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
